e7786e948f5963bb0fc8574118e21c63fe645ea7
Jeremy <jeremy.aubert@vodalys.com>
2014-09-05 15:48:34 +0200

    disable default right click

--- app/js/ninja/worldCreate.js
+++ app/js/ninja/worldCreate.js
@@ -5,6 +5,7 @@ define(function worldCreateFactory() {
         game.stage.backgroundColor = 0x000000;
         game.physics.startSystem(Phaser.Physics.NINJA);
         game.physics.ninja.gravity = 0.5;
+        game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
 
 
         //  The 'mario' key here is the Loader key given in game.load.tilemap
